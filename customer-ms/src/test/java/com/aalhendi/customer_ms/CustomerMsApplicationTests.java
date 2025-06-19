09df5b73e483bd444dc84c84e2b6c735685123c6
Abdulrazaq Alhendi <dev@aalhendi.com>
2025-06-19 21:16:17 +0300

    init: add microservice boilerplate

---
+++ customer-ms/src/test/java/com/aalhendi/customer_ms/CustomerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aalhendi.customer_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
